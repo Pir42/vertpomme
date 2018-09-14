d1a033ff971af73d9524072cfe28c329e2dd24a7
Pierre Moinet <pierre.moinet44600@gmail.com>
2018-09-14 09:52:42 +0200

    change js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(".box").mouseover(function(){
 
 var testing = 0;
 
-$("#two").mouseon(function(){
+$("#two").mouseover(function(){
 
     $("#two > h3").css("opacity", "1");
     $("#liane2").css("left", "-300px");
